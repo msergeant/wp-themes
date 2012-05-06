f24de2d9029ec11030e6a2480c633b01559ed078
Mike Sergeant <sergeant.mj@gmail.com>
2012-05-06 10:21:05 -0400

    Pretty much done with the header

--- TPNeurology/header.php
+++ TPNeurology/header.php
@@ -8,47 +8,56 @@
 	
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/base.css" type="text/css" media="screen" charset="utf-8" />
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/theme.css" type="text/css" media="screen" charset="utf-8" />
-
 	<?php wp_head(); ?>
+
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/scripts/site.js"></script>
 	
 </head>
 <body <?php body_class(); ?>>
 	<div class="container">
 		<div id="header">
 			<div id="nav">
-                <h3>About TPN</h3>
-                <ul class="nav-menu">
-                	<li>
-                		<a href="#">Our Story</a>
-                	</li>
-                	<li>
-                		<a href="#">Meet Dr. Sergeant</a>
-                	</li>
-                	<li>
-                		<a href="#">Accepted Insurances</a>
-                	</li>
+            <ul class="nav-menu">
+                    <li class="nav-menu-head"><h3>About TPN</h3>
+                        <ul class="actions">
+                            <li>
+                                <a href="#">Our Story</a>
+                            </li>
+                            <li>
+                                <a href="#">Meet Dr. Sergeant</a>
+                            </li>
+                            <li>
+                                <a href="#">Accepted Insurances</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-menu-head"><h3>Services</h3>
+                        <ul class="actions">
+                            <li>
+                                <a href="#">TBD</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-menu-head"><h3>Contact Us</h3>
+                        <ul class="actions">
+                            <li>
+                                <a href="#">Contact Info</a>
+                            </li>
+                            <li>
+                                <a href="#">Directions</a>
+                            </li>
+                            <li>
+                                <a href="#">Office Hours</a>
+                            </li>
+                        </ul>
+                    </li>
                 </ul>
-                <h3>Services</h3>
-                <ul class="nav-menu">
-                	<li>
-                		<a href="#">TBD</a>
-                	</li>
-                </ul>
-                <h3>Contact Us</h3>
-                <ul class="nav-menu">
-                	<li>
-                		<a href="#">Contact Info</a>
-                	</li>
-                	<li>
-                		<a href="#">Directions</a>
-                	</li>
-                	<li>
-                		<a href="#">Office Hours</a>
-                	</li>
-                </ul>
-			</div>
+                </div>
             <div class="logo">
+                <a href="/"><img src="<?php echo get_template_directory_uri(); ?>/images/tpn_web.jpg" alt="Tower Plaza Neurology" /></a>
             </div>
+			</div>
 		</div>
 		<div id="content">
 			<?php // Content ?>
---
+++ TPNeurology/scripts/site.js
@@ -0,0 +1,22 @@
+(function($){
+    var timeout         = 500;
+    var closetimer      = 0;
+    var ddmenuitem      = 0;
+    function menu_open(){
+        menu_canceltimer();
+        menu_close();
+        ddmenuitem = $(this).find('ul').eq(0).css('visibility', 'visible');}
+    function menu_close(){
+        if(ddmenuitem) ddmenuitem.css('visibility', 'hidden');}
+    function menu_timer(){
+        closetimer = window.setTimeout(menu_close, timeout);}
+    function menu_canceltimer(){
+        if(closetimer){
+        window.clearTimeout(closetimer);
+        closetimer = null;}}
+    $(document).ready(function(){
+        $('.nav-menu > li').bind('mouseover', menu_open);
+        $('.nav-menu > li').bind('mouseout',  menu_timer);
+        $('.nav-menu > li > ul').css('visibility', 'hidden');});
+    document.onclick = menu_close;
+})(jQuery);
