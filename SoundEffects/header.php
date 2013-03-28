900eb5b7b0bcfcc769a44bf45710548b7d81def1
Mike Sergeant <sergeant.mj@gmail.com>
2013-03-27 20:31:21 -0400

    Add description to the header

--- SoundEffects/header.php
+++ SoundEffects/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title><?php wp_title('|',1,'right'); ?> <?php bloginfo('name'); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
+    <meta name="description" content="Play these poker sounds at your next home game and you're almost guaranteed to put somebody on tilt.">
     <meta name="author" content="">
     <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
 
